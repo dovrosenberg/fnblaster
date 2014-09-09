75ced1dcbd3ba62c49489c5cee595dbb44e33f9d
dovrosenberg <dov@rosenbergmail.com>
2014-09-09 16:38:11 -0400

    changed server.js

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-var express = require('express')();
-var app = express.createServer();
-express.use(express.static('app'));
-express.listen(process.env.PORT);
+var express = require('express');
+var app = express();
+
+app.get('/', function(req,res) {
+   res.send('hello world');
+});
+
+/*express.use(express.static('app'));*/
+app.listen(process.env.PORT);
