32ea6c06a0f95be55b3e0fcdaf64b230915e2dbb
dovrosenberg <dov@rosenbergmail.com>
2014-09-09 16:42:15 -0400

    no message

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
+app.use(express.static('/app'));
+/*app.get('/', function(req,res) {
    res.send('hello world');
 });
 
-/*express.use(express.static('app'));*/
-app.listen(process.env.PORT);
+
+app.listen(process.env.PORT);*/
