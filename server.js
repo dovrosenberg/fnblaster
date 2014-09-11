10a8dffc01313d5d9a2eb8a11b6c4a424fb2f0d0
dovrosenberg <dov@rosenbergmail.com>
2014-09-11 17:07:19 -0400

    still trying to get it to work

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('__dirname'));
+//app.use(express.static('__dirname'));
 /*app.get('/', function(req,res) {
    res.send('hello world');
 });
 */
 
-app.listen(process.env.PORT);
+//app.listen(process.env.PORT);
+
+app.configure(function() {
+	app.set('port', process.env.PORT || 3000);
+	app.set('view options', {layout: false});
+	app.engine('html', require('ejs').renderFile);
+	app.use(app.router);
+});
