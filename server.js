685080b45e08d4d3e92055e246cd10fe12f02aaa
dovrosenberg <dov@rosenbergmail.com>
2014-09-11 17:54:22 -0400

    still trying to get it to work

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static('/game'));
+app.use(express.static(__dirname + '/app'));
 /*app.get('/', function(req,res) {
    res.send('hello world');
 });
 */
 
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
